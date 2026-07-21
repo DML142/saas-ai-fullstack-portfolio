8ea0569e7bbfcbb52210dadf6f0b025ffbe6bb56
DML_142 <demolovfennec@gmail.com>
2026-07-21 22:03:44 +0300

    six(eslint): commented no unsafe argument rule for frontend

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -27,9 +27,9 @@ const eslintConfig = defineConfig([
 
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      // "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-floating-promises": "warn",
-      "@typescript-eslint/no-unsafe-argument": "warn",
+      // "@typescript-eslint/no-unsafe-argument": "warn",
       "prettier/prettier": ["error", { endOfLine: "auto" }],
       "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
     },
